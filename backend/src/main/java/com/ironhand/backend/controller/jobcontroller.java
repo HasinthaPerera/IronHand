07618f3a4ba6f78935acd3cce61b0d8fb92fdb91
HasinthaPerera <hasinthaheshan15@gmail.com>
2026-06-07 22:12:47 +0530

    Enhance homepage & controller, hide sensitive database configuration

--- backend/src/main/java/com/ironhand/backend/controller/jobcontroller.java
+++ backend/src/main/java/com/ironhand/backend/controller/jobcontroller.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/jobs")
 @CrossOrigin(origins = "*")
@@ -21,5 +23,10 @@ public class jobcontroller {
 
         jobRepository.save(jobData);
         return ResponseEntity.ok("Job posted successfully!");
+    }
+
+    @GetMapping("/all")
+    public List<job> getAllJobs() {
+        return jobRepository.findAll();
     }
 }
